c6c30222a90fa8cd5cfa246c321f0762a615ed17
Mrmaxmeier <Mrmaxmeier@gmail.com>
2018-08-31 10:42:20 +0200

    implement undo

--- src/GameVis.tsx
+++ src/GameVis.tsx
@@ -1,127 +1,103 @@
-import * as React from "react";
+import * as React from 'react';
 
-import { IGameProtocol, IField, IStanding, IStep } from "./protocol";
+import { IGameProtocol, IField, IStanding } from './protocol';
 
-import { GameGrid } from "./GameGrid";
-import { FieldColors } from "./FieldVis";
+import { GameGrid } from './GameGrid';
+import { FieldColors } from './FieldVis';
+import { StepManager } from './StepManager';
 
 interface State {
-  standings: IStanding[];
-  fields: IField[];
-  step: number;
-  stepTimer?: number;
+	standings: IStanding[];
+	fields: IField[];
+	currentStepIndex: number;
+	stepTimer?: number;
 }
 
 interface Props {
-  size: number;
-  game: IGameProtocol;
+	size: number;
+	game: IGameProtocol;
 }
 
 export class GameVis extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = this.getInitialState(props.game);
-    this.toggleAutoStep = this.toggleAutoStep.bind(this);
-  }
+	private stepManager: StepManager;
 
-  componentDidUpdate(prevProps: Props) {
-    if (prevProps.game !== this.props.game) {
-      this.setState(this.getInitialState(this.props.game));
-    }
-  }
+	constructor(props: Props) {
+		super(props);
+		this.toggleAutoStep = this.toggleAutoStep.bind(this);
+		this.stepManager = new StepManager(props.game);
+		this.state = this.stepManager.getState();
+	}
 
-  getInitialState(game: IGameProtocol): State {
-    return {
-      step: 0,
-      standings: game.steps.length ? game.steps[0].standings : [],
-      fields: game.init.fields
-    };
-  }
+	componentDidUpdate(prevProps: Props) {
+		if (prevProps.game !== this.props.game) {
+			this.stepManager = new StepManager(this.props.game);
+			this.setState(this.stepManager.getState());
+		}
+	}
 
-  apply(step: IStep, index: number): void {
-    let fields = this.state.fields.slice();
-    step.fields.forEach(f => {
-      let i = fields.findIndex(o => f.x === o.x && f.y === o.y);
-      fields[i] = f;
-    });
-    this.setState({
-      standings: step.standings,
-      fields,
-      step: index + 1
-    });
-  }
+	toggleAutoStep() {
+		if (this.state.stepTimer) {
+			clearInterval(this.state.stepTimer);
+			this.setState({ stepTimer: undefined });
+		} else {
+			const timer = setInterval(() => {
+				let next = this.state.currentStepIndex;
+				if (next >= this.props.game.steps.length) {
+					this.toggleAutoStep();
+				} else {
+					this.setState(this.stepManager.next());
+				}
+			}, 100);
+			this.setState({ stepTimer: (timer as any) as number });
+		}
+	}
 
-  toggleAutoStep() {
-    if (this.state.stepTimer) {
-      clearInterval(this.state.stepTimer);
-      this.setState({ stepTimer: undefined });
-    } else {
-      const timer = setInterval(() => {
-        let next = this.state.step;
-        if (next >= this.props.game.steps.length) {
-          this.toggleAutoStep();
-        } else {
-          this.apply(this.props.game.steps[next], next);
-        }
-      }, 100);
-      this.setState({ stepTimer: (timer as any) as number });
-    }
-  }
-
-  render() {
-    const { width, height } = this.props.game.init;
-    return (
-      <div style={{ width: "100%", height: "100%" }}>
-        <div>GameVis memes</div>
-        {this.state.standings.length ? (
-          <table style={{ textAlign: "center" }}>
-            <tbody>
-              <tr>
-                <th>Swarm</th>
-                <th>Color</th>
-                <th>Score</th>
-                <th>Ants</th>
-              </tr>
-              {this.state.standings.map(({ score, swarm_id, ants }, i) => (
-                <tr key={i}>
-                  <td>{swarm_id}</td>
-                  <td style={{ backgroundColor: FieldColors[swarm_id] }} />
-                  <td>{score}</td>
-                  <td>{ants}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        ) : null}
-        <div>
-          Step: {this.state.step} / {this.props.game.steps.length}
-        </div>
-        <button
-          onClick={() => {
-            this.setState(this.getInitialState(this.props.game));
-          }}
-        >
-          Reset Game
-        </button>
-        <button disabled={true}>Prev Step</button>
-        <button onClick={this.toggleAutoStep}>
-          {this.state.stepTimer ? "Stop" : "Play"}
-        </button>
-        <button
-          onClick={() => {
-            this.apply(this.props.game.steps[this.state.step], this.state.step);
-          }}
-          disabled={this.state.step === this.props.game.steps.length}
-        >
-          Next Step
-        </button>
-        <GameGrid
-          size={this.props.size}
-          fields={this.state.fields}
-          width={width}
-          height={height}
-        />
-      </div>
-    );
-  }
+	render() {
+		const { width, height } = this.props.game.init;
+		return (
+			<div style={{ width: '100%', height: '100%' }}>
+				<div>GameVis memes</div>
+				{this.state.standings.length ? (
+					<table style={{ textAlign: 'center' }}>
+						<tbody>
+							<tr>
+								<th>Swarm</th>
+								<th>Color</th>
+								<th>Score</th>
+								<th>Ants</th>
+							</tr>
+							{this.state.standings.map(({ score, swarm_id, ants }, i) => (
+								<tr key={i}>
+									<td>{swarm_id}</td>
+									<td style={{ backgroundColor: FieldColors[swarm_id] }} />
+									<td>{score}</td>
+									<td>{ants}</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				) : null}
+				<div>
+					Step: {this.state.currentStepIndex} / {this.props.game.steps.length}
+				</div>
+				<button
+					onClick={() => {
+						this.stepManager = new StepManager(this.props.game);
+						this.setState(this.stepManager.getState());
+					}}
+				>
+					Reset Game
+				</button>
+				<button disabled={!this.stepManager.hasPrev()} onClick={() => this.setState(this.stepManager.prev())}>
+					Prev Step
+				</button>
+				<button onClick={this.toggleAutoStep}>{this.state.stepTimer ? 'Stop' : 'Play'}</button>
+				<button onClick={() => this.setState(this.stepManager.next())} disabled={!this.stepManager.hasNext()}>
+					Next Step
+				</button>
+				<br />
+				<GameGrid size={this.props.size} fields={this.state.fields} width={width} height={height} />
+			</div>
+		);
+	}
 }
---
+++ src/StepManager.ts
@@ -0,0 +1,69 @@
+import { IGameProtocol, IField, IStanding } from './protocol';
+
+interface GameState {
+	standings: IStanding[];
+	fields: IField[];
+	currentStepIndex: number;
+}
+
+export class StepManager {
+	private game: IGameProtocol;
+	private state: GameState;
+	private undo: IField[][] = [];
+
+	constructor(game: IGameProtocol) {
+		this.game = game;
+		this.state = {
+			standings: game.steps.length ? game.steps[0].standings : [],
+			fields: game.init.fields,
+			currentStepIndex: 0
+		};
+	}
+
+	hasNext(): boolean {
+		return this.state.currentStepIndex < this.game.steps.length;
+	}
+	hasPrev(): boolean {
+		return this.state.currentStepIndex > 0 && this.state.currentStepIndex <= this.undo.length;
+	}
+
+	applyFieldChange(orig: IField[], changes: IField[]): IField[] {
+		let fields = orig.slice();
+		changes.forEach((f) => {
+			let i = fields.findIndex((o) => f.x === o.x && f.y === o.y);
+			fields[i] = f;
+		});
+		return fields;
+	}
+
+	next(): GameState {
+		let step = this.game.steps[this.state.currentStepIndex];
+
+		if (this.state.currentStepIndex >= this.undo.length) {
+			// build undo buffer
+			let undo = step.fields.map(
+				({ x, y }) => this.state.fields.find((other) => other.x === x && other.y === y)!
+			);
+			this.undo.push(undo);
+		}
+
+		let fields = this.applyFieldChange(this.state.fields, step.fields);
+		let standings = step.standings;
+
+		this.state = { fields, standings, currentStepIndex: this.state.currentStepIndex + 1 };
+		return this.state;
+	}
+
+	prev(): GameState {
+		let currentStepIndex = this.state.currentStepIndex - 1;
+		let { standings } = this.game.steps[currentStepIndex];
+		let fields = this.applyFieldChange(this.state.fields, this.undo[currentStepIndex]);
+
+		this.state = { fields, standings, currentStepIndex };
+		return this.state;
+	}
+
+	getState() {
+		return this.state;
+	}
+}
